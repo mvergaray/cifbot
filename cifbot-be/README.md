952ce0289323e819c45bb20227d17bf3ed541d45
mvergaray <marco.vergaray@hotmail.com>
2019-12-10 20:01:03 -0500

    WIP

--- cifbot-be/README.md
+++ cifbot-be/README.md
@@ -2,4 +2,5 @@ Requirements:
 
 npm install -g forever
 
+cd ~/cifbot/cifbot-be
 sudo forever -o log/out.log -e log/err.log start -c "node --stack_size=4096 --max-old-space-size=4096" app.js
--- cifbot-be/controllers/webhook.js
+++ cifbot-be/controllers/webhook.js
@@ -3,13 +3,23 @@ let Controller = {};
 
 Controller.handleWebhook = (req, res) => {
   console.log('handle webhook');
-  console.log(req);
+  console.log('WH Body:');
+  console.log(req.body);
+  console.log('WH Query:');
+  console.log(req.query);
+  console.log('WH Params:');
+  console.log(req.params);
   return res.status(200).send('handleWebhook - GET');
 };
 
 Controller.handleWebhookPost = (req, res) => {
   console.log('handle webhook post');
-  console.log(req);
+  console.log('WH Body:');
+  console.log(req.body);
+  console.log('WH Query:');
+  console.log(req.query);
+  console.log('WH Params:');
+  console.log(req.params);
   return res.status(200).send('handleWebhook - POST');
 };
 
