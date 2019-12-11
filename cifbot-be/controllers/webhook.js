a02b9163d9c1ec3469665f9de4fc42edf75fdda9
Marco Vergaray <marco.vergaray@hotmail.com>
2019-12-10 23:35:12 -0500

    wip

--- cifbot-be/controllers/webhook.js
+++ cifbot-be/controllers/webhook.js
@@ -83,8 +83,23 @@ Controller.handleWebhookPost = (req, res) => {
     Total de dinero que salió: S/.${results.outcomeTotalAmount}\n\n
     `;
 
+    var agentResponse = [
+      `Este es el estado de cuentas de compras y ventas durante el mes de Diciembre: `,
+      `Facturas de Venta:`,
+      `# de facturas: ${results.salesCount}\n\n`,
+      `# de facturas cobradas: ${results.salesCompletedCount}\n\n`,
+      `Total de dinero emitido por las facturas: ${results.salesTotalAmount}\n\n`,
+      `Total de dinero que ingresó: ${results.incomeTotalAmount}\n\n`,
+      `Facturas e Compra:`,
+      `# de facturas: ${results.purchaseCount}\n\n`,
+      `# de facturas pagadas: ${results.purchasesCompletedCount}\n\n`,
+      `Total de dinero emitido por las facturas ${results.salesTotalAmount} \n\n`,
+      `otal de dinero que salió: S/.${results.outcomeTotalAmount}\n\n`
+    ];
+
     return res.status(200).send({
-      result: resultText
+      result: resultText,
+      agentResponse: agentResponse
     });
   })
   .catch((err)=> {
