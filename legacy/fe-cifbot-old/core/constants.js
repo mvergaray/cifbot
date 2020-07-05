320a10352ae49c2dc588071d63b8c559a3f3b228
Marco Vergaray <marco.vergaray@hotmail.com>
2020-07-05 17:39:33 -0500

    Remove legacy

--- legacy/fe-cifbot-old/core/constants.js
+++
@@ -1,8 +0,0 @@
-(() => {
-  'use strict';
-
-  angular.module('documentarioApp')
-    .constant('_', window._)
-    .constant('$', window.jQuery)
-    .constant('moment', window.moment);
-})();
--- legacy/frontend/src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-    production: true,
-    apiSuffix: 'http://198.199.65.100/api',
-    tokenName: 'cifbot-token'
-};
